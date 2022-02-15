297c339edf7e1f1c3cc4164c2004aca96446a6f3
Holger <86824184+HolgerSin@users.noreply.github.com>
2022-02-15 16:59:03 +0100

    Maven Demo

---
+++ 05_MavenDemo/05_MavenDemo/src/test/java/com/cc_student/hsin/_MavenDemo/AppTest.java
@@ -0,0 +1,27 @@
+package com.cc_student.hsin._MavenDemo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+//    
+//    @Test
+//    public void shouldFail()
+//    {
+//        fail("my testfail");
+//    }
+}
